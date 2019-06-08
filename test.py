0f329057ea78c44859e587ccef969961d03ad037
enaosun <enao_sun@163.com>
2019-06-08 18:23:09 +0800

    try

--- test.py
+++ test.py
@@ -5,7 +5,11 @@ os.system('cd Cloud-Platform && ls')
 #os.system ('git config --global user.name "enaosun"')
 #os.system('git config --global user.email "enao_sun@163.com"')
 #os.system('git pull origin master')
+os.system('git fetch origin')
+os.system('git merge origin/master')
 os.system('git add .')
+
 #os.system('git remote add origin "https://github.com/vyychenyy/Cloud-Platform.git"')
 os.system('git commit -m "try"')
+
 os.system('git branch --set-upstream-to=origin/<branch> master')
