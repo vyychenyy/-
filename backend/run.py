b41a46a7a71036ceda245715d862882f5e2e548b
enao_sun <37493348+enaosun@users.noreply.github.com>
2019-06-20 21:10:19 +0800

    add run.py

---
+++ backend/run.py
@@ -0,0 +1,5 @@
+from maintenanceapp import app
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '172.20.10.2',port = '8089')
+    #app.run(debug = True, host = 'localhost',port = '8089')
